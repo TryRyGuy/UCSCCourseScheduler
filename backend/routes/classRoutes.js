43714b5c4db8777cfc83140c82324c2f06071bc3
Nathan Le <nathanle2002@gmail.com>
2024-08-22 16:49:29 -0700

    Update classRoutes.js
    Should be able to drop classes now and update the backend

--- backend/routes/classRoutes.js
+++ backend/routes/classRoutes.js
@@ -6,26 +6,48 @@ const ClassInfo = require('../models/classInfo');
 
 router.get('/addClasses', async (req, res) => {
     const { classId, scheduleId } = req.body;
-
     try {
         // Ensure the user is logged in and has a session
         if (!req.session.user) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
-
         // Find the user and their schedules
         const user = await User.findOne({ _id: req.session.user._id }).populate('scheduleId');
         const schedules = user.scheduleId;
-
         // Find the specific schedule to update
         const specSched = schedules.find(sch => sch._id.equals(scheduleId));
-
         if (!specSched) {
             return res.status(404).json({ message: 'Schedule not found for the user' });
         }
         const updatedSchedule = await Schedule.findById(specSched);
         if (updatedSchedule) {
             updatedSchedule.classes.push({ classId, isUsed: false });
+            await updatedSchedule.save();
+        }
+        res.status(200).json({ message: 'Class added to schedule', theSchedule });
+    } catch (error) {
+        res.status(500).json({ message: 'Error adding class to schedule', error: error.message });
+    }
+});
+
+router.get('/dropClasses', async (req, res) => {
+    const { classId, scheduleId } = req.body;
+    try {
+        // Ensure the user is logged in and has a session
+        if (!req.session.user) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+        // Find the user and their schedules
+        const user = await User.findOne({ _id: req.session.user._id }).populate('scheduleId');
+        const schedules = user.scheduleId;
+        // Find the specific schedule to update
+        const specSched = schedules.find(sch => sch._id.equals(scheduleId));
+        if (!specSched) {
+            return res.status(404).json({ message: 'Schedule not found for the user' });
+        }
+        const updatedSchedule = await Schedule.findById(specSched);
+        if (updatedSchedule) {
+            updatedSchedule.classes = updatedSchedule.classes.filter(cls => cls.classId.toString() !== classId.toString());
             await updatedSchedule.save();
         }
         res.status(200).json({ message: 'Class added to schedule', theSchedule });
